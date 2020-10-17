f969967c72a96aea61c74790676b2f39b76038e3
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-17 16:14:29 -0500

    258 Buscador de peliculas

--- src/app/pipes/poster.pipe.ts
+++ src/app/pipes/poster.pipe.ts
@@ -7,7 +7,7 @@ export class PosterPipe implements PipeTransform {
 
   transform(poster: string ): string {
 
-    if( poster ) {
+    if ( poster ) {
       return `https://image.tmdb.org/t/p/w500${ poster }`;
     } else {
       return './assets/no-image.jpg';
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -42,7 +42,17 @@ export class PeliculasService {
         this.cargando = false;
       })
     );
+  }
 
+  buscarPeliculas( texto: string ): Observable<Movie[]> {
+
+    const params = {...this.params, page: '1', query: texto};
+
+    return this.http.get<CarteleraResponse>(`${ this.baseUrl }/search/movie`, {
+      params
+    }).pipe(
+      map( resp => resp.results )
+    );
   }
 
 }
