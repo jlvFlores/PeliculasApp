6207f29b577da57792ad9ec7895dcf8c21fe817d
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-17 14:20:20 -0500

    257 Poster Pipe

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,7 @@ import { RatingModule } from 'ng-starrating';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-
   imports: [
     CommonModule,
     RouterModule,
-    RatingModule
+    RatingModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
